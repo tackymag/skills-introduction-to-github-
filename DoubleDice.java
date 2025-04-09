345caf4265f09abf65b6770d66cbc214d07e8ac3
tackymag <rohit.swaroop12@gmail.com>
2025-04-09 11:34:00 -0700

    Update DoubleDice.java

--- DoubleDice.java
+++ DoubleDice.java
@@ -1,8 +1,4 @@
 import java.util.Scanner
 
 public class DoubleDice {
-  public static void main(String[] args) {
-    System.out.println("Hello, world");
-  }
-}
-    
+  public static void main(String[] args) {   
