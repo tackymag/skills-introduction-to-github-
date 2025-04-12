cd7c4dfc583e3a3a164f74bb49df414a055008f4
tackymag <rohit.swaroop12@gmail.com>
2025-04-11 17:30:03 -0700

    Update Die.java

--- Die.java
+++ Die.java
@@ -11,3 +11,31 @@ public class Die {
   public void roll() {
     value = randGen.nextInt(6) + 1;
   }
+
+  @Override
+  public boolean equals(Die die2) {
+    return this.value == die2.value;
+  }
+
+  @Override
+  public String toString() {
+    if (value == 1) {
+      return "one";
+    }
+    else if (value == 2) {
+      return "two";
+    }
+    else if (value == 3) {
+      return "three";
+    }
+    else if (value == 4) {
+      return "four";
+    }
+    else if (value == 5) {
+      return "five";
+    }
+    else if (value == 6) {
+      return "six";
+    }
+  }
+    
