b4bc8ea59d0164a0c2b63e8fbbebe460145365ee
tackymag <rohit.swaroop12@gmail.com>
2025-04-12 11:59:21 -0700

    Update DoubleDice.java

--- DoubleDice.java
+++ DoubleDice.java
@@ -1,4 +1,13 @@
 import java.util.Scanner
 
 public class DoubleDice {
-  public static void main(String[] args) {   
+  public static void main(String[] args) {
+    Scanner scnr = new Scanner(System.in);
+    Die die1 = new Die();
+    Die die2 = new Die();
+    
+    double money = 100.00;
+    
+    while (Math.abs(money - 0.0) > 0.000001) {
+      // Do something
+    }
