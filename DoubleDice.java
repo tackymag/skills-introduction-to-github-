c1d04bd8fd082b66371f9e8c5752d6f4ab807b6e
tackymag <rohit.swaroop12@gmail.com>
2025-04-12 18:21:33 -0700

    Update DoubleDice.java

--- DoubleDice.java
+++ DoubleDice.java
@@ -10,13 +10,16 @@ public class DoubleDice {
     double money = 100.00;
     double betAmount;
     
+    // Using the floating-point comparison technique to avoid small differences
     while (Math.abs(money - 0.0) > 0.000001) {
-      System.out.printf("You have $%.2f/n", money);
+      System.out.printf("You have $%.2f\n", money);
       System.out.println();
       System.out.println("How much would you like to bet (Enter 0 to quit)?");
 
+      // The try-catch block stops the program from crashing in case of a non-numeric input
       try {
         betAmount = scnr.nextDouble();
+        scnr.nextLine();
 
         if (betAmount == 0.0) {
           System.out.println("See you around, winner!");
@@ -33,27 +36,32 @@ public class DoubleDice {
           continue;
         }
       }
-      catch (NumberFormatException e) {
+      catch (InputMismatchException e) {
         System.out.println("Please enter a numeric bet");
+        scnr.nextLine();
+        continue;
       }
 
+      // Roll the dice
       die1.roll();
       die2.roll();
 
-      System.out.println("You rolled a " + die1.toString(); + " and " + die2.toString());
+      System.out.println("You rolled a " + die1.toString() + " and " + die2.toString());
 
       if (die1.equals(die2)) {
-        System.out.println("You win %.2f/n", betAmount);
+        System.out.printf("You win $%.2f\n", betAmount);
         money += betAmount;
       }
       else {
-        System.out.println("You lose %.2f/n", betAmount);
+        System.out.printf("You lose $%.2f\n", betAmount);
         money -= betAmount;
       }
 
       System.out.println();
+      System.out.println();
     }
 
+    // Executes is player is out of money after the game ends
     if (money <= 0.0) {
       System.out.println("You are out of money!");
       System.out.println("Better luck next time");
